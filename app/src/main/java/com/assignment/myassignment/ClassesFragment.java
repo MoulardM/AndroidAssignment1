1591b5326fab3d5dd10e4d5b7df541e7b4e7ae37
maxime.moulard <maxime.moulard@epitech.eu>
2018-11-09 11:56:44 +0100

    New

---
+++ app/src/main/java/com/assignment/myassignment/ClassesFragment.java
@@ -0,0 +1,20 @@
+package com.assignment.myassignment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by maxime.moulard on 09/11/2018.
+ */
+
+public class ClassesFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_classes, null);
+    }
+}
